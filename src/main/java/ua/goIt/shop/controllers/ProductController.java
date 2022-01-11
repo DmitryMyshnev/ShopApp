4448b4174a5b91696f3f900ad370ccbf4f993c36
Дмитрий Мишнев <dmitrymishnev@gmail.com>
2022-01-11 15:36:36 +0200

    upd

--- src/main/java/ua/goIt/shop/controllers/ProductController.java
+++ src/main/java/ua/goIt/shop/controllers/ProductController.java
@@ -6,7 +6,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import ua.goIt.shop.model.Manufacturer;
 import ua.goIt.shop.model.Product;
